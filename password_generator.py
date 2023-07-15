c2c6f36c236db67a11661836e4fb7f0b874e748d
Aanand Aman <109314074+Aanand058@users.noreply.github.com>
2023-07-15 06:13:55 -0400

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,1 +1,17 @@
+import random
 
+alphabets="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+numbers = "1234567890"
+symbols = "!@#$%^&*()?"
+
+letterLen = int(input("Enter how many letters you want in your password: "))
+numLen = int(input("Enter how many digits you want in your password: "))
+symbolLen = int(input("Enter how many symbols you want in your password: "))
+
+#Generating different variations of required password
+p1 = "".join(random.sample(alphabets,letterLen))
+p2 = "".join(random.sample(numbers,numLen))
+p3 = "".join(random.sample(symbols,symbolLen))
+
+#Printing the combined password
+print(p1+p2+p3)
